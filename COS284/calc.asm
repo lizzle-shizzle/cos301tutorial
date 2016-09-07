fb79683c3e83429d920a7d40cf930816eaea511a
u14310156 <u14310156@tuks.co.za>
2016-09-07 13:57:38 +0200

    Add files via upload

--- COS284/calc.asm
+++ COS284/calc.asm
@@ -1,58 +1,232 @@
-DATA SEGMENT
-     NUM1 DB ?
-     NUM2 DB ?
-     RESULT DB ?
-     MSG1 DB 10,13,"ENTER FIRST NUMBER TO MULTIPLY : $"
-     MSG2 DB 10,13,"ENTER SECOND NUMBER TO MULTIPLY : $"  
-     MSG3 DB 10,13,"RESULT OF MULTIPLICATION IS : $"
-ENDS
-CODE SEGMENT 
-    ASSUME DS:DATA CS:CODE
-START:
-      MOV AX,DATA
-      MOV DS,AX
-     
-      LEA DX,MSG1
-      MOV AH,9
-      INT 21H
-     
-      MOV AH,1
-      INT 21H
-      SUB AL,30H
-      MOV NUM1,AL
-     
-      LEA DX,MSG2
-      MOV AH,9
-      INT 21H
-     
-      MOV AH,1
-      INT 21H
-      SUB AL,30H
-      MOV NUM2,AL
-     
-      MUL NUM1
-              
-      MOV RESULT,AL
-      AAM
-     
-      ADD AH,30H
-      ADD AL,30H 
-     
-      MOV BX,AX
-     
-      LEA DX,MSG3
-      MOV AH,9
-      INT 21H
-     
-      MOV AH,2
-      MOV DL,BH
-      INT 21H
-     
-      MOV AH,2
-      MOV DL,BL
-      INT 21H
-     
-      MOV AH,4CH
-      INT 21H     
-ENDS
-END START
+section .data	
+	space db ' '
+	r db 'r'
+	plus db '+'
+	minus db '-'
+	newLine db 10
+	value1 dq 0
+
+segment .bss
+num resq 1
+
+var resb 1
+sign resb 1
+value2 resd 1
+
+
+	
+segment .text
+global _start
+_start:
+
+	xor r8,r8
+	mov r9,7
+	mov r10,10
+	xor r15,r15
+
+read:
+	
+	xor eax,eax
+	xor edi,edi
+	mov rsi, var
+	mov rdx, 1
+	syscall
+	
+	dec r9
+	mov r14,r9
+	movsx rax,byte[var]
+	sub rax,'0'
+	cmp r9,0
+	je save
+	cmp rax,0
+	je read
+	jmp digit
+	
+digit:
+	cmp r14,0
+	je hold 
+	;mov rax, qword[num]
+	mul r10
+	;mov [num], rax
+	dec r14
+	jmp digit
+
+hold:
+	add r15,rax
+	jmp read
+	
+save:	
+	add r15,rax
+	cmp r8,0
+	je savefirst
+	mov [value2],r15
+	
+	
+	
+	jmp calc
+	
+savefirst:	
+	inc r8
+	
+	
+	
+	mov [value1],r15
+	
+	mov r9,7
+	xor r15,r15
+	
+	jmp getSign
+	
+getSign:	
+	xor eax,eax
+	xor edi,edi
+	mov rsi, var
+	mov rdx, 1
+	syscall
+	
+	xor eax,eax
+	xor edi,edi
+	mov rsi, sign
+	mov rdx, 1
+	syscall
+	
+	xor eax,eax
+	xor edi,edi
+	mov rsi, var
+	mov rdx, 1
+	syscall
+	jmp read
+
+negative:
+	neg qword[num]
+	mov rax,1
+	mov rdi,1
+	mov rdx,1
+	mov rsi,minus
+	syscall
+	mov r14,15
+	
+	jmp toDec
+
+
+calc:
+	xor r15,r15
+	
+	mov rax, [value1]
+	movzx r8,byte [sign]
+	
+	cmp r8,'-'
+	je subing
+	cmp r8,'*'
+	je muling
+	cmp r8,'/'
+	je diving
+	cmp r8,'+'
+	jne terminate
+;adding
+	
+	add rax,[value2]
+	
+	;mov rax, 12358656
+	mov [num],rax
+	jmp positive
+subing:
+	sub rax,[value2]
+	mov [num],rax
+	cmp rax,0
+	jl negative
+	jmp positive
+muling:
+	mov r9,[value2]
+	mul r9
+	mov [num],rax
+	jmp positive
+diving:	
+	xor edx,edx
+	mov r9,[value2]
+	div r9
+	mov [num],rax
+	mov r15,rdx
+
+positive:
+	mov rax,1
+	mov rdi,1
+	mov rdx,1
+	mov rsi,plus
+	syscall
+	mov r14,15 
+	
+toDec:
+	cmp r14,0
+	je print 
+	
+	mov r9,r14
+	mov rax, [num] ;value to output as dec
+	dec r14
+	
+	
+forloop:
+	xor edx,edx
+	div r10
+	sub r9,1
+	cmp r9,0
+	jne forloop
+	
+	
+	add rdx,'0'
+	mov [var],rdx
+	mov rax,1
+	mov rdi,1
+	mov rdx,1
+	mov rsi,var
+	syscall
+	jmp toDec
+	
+print:
+	
+	
+	cmp r15,0
+	je terminate
+	mov rax,1
+	mov rdi,1
+	mov rdx,1
+	mov rsi,space
+	syscall
+	
+	
+	mov rax,1
+	mov rdi,1
+	mov rdx,1
+	mov rsi,r
+	syscall
+	
+	mov rax,1
+	mov rdi,1
+	mov rdx,1
+	mov rsi,space
+	syscall
+	
+	add r15,'0'
+	mov [var],r15
+	mov rax,1
+	mov rdi,1
+	mov rdx,1
+	mov rsi,var
+	syscall
+
+terminate:	
+	mov rax,1
+	mov rdi,1
+	mov rdx,1
+	mov rsi,newLine
+	syscall
+	
+	xor eax,eax
+	xor edi,edi
+	mov rsi, var
+	mov rdx, 2
+	syscall
+	
+	mov rax,60
+	xor edi,edi
+	syscall
